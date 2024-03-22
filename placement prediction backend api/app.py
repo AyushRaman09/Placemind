dc9a15e42db19f6c703937775244cfffad843177
AyushRaman09 <12102120601008@adit.ac.in>
2024-03-22 23:14:15 +0530

    app update

--- placement prediction backend api/app.py
+++ placement prediction backend api/app.py
@@ -28,44 +28,35 @@ def index():
     return {
         'message' : "Hello, Stranger"
     }
-
-
-@app.get('/{name}')
-def get_name(name:str):
-    return {
-        'message' : f'Hello, {name}'
-    }
-
 @app.post('/predict')
-def predict_placement(data:attrName):
+def predict_placement(data: attrName):
     data = dict(data)
     print(data)
-    Age = data['Age']                  
-    Gender  = data['Gender']              
-    Stream = data['Stream']               
-    Internships = data['Internships']          
-    CGPA = data['CGPA']                 
-    Hostel  = data['Hostel']              
-    HistoryOfBacklogs = data['HistoryOfBacklogs']    
-    projectsCount  = data['projectsCount']
+    Age = int(data['Age'])
+    Gender = int(data['Gender'])
+    Stream = int(data['Stream'])
+    Internships = int(data['Internships'])
+    CGPA = float(data['CGPA'])
+    Hostel = int(data['Hostel'])
+    HistoryOfBacklogs = int(data['HistoryOfBacklogs'])
+    projectsCount = int(data['projectsCount'])
 
     prediction_from_models = []
 
-    prediction_from_models.append(LGmodel.predict([[Age,Gender,Stream,Internships,CGPA,Hostel,HistoryOfBacklogs,projectsCount]])[0])
-    prediction_from_models.append(KNNmodel.predict([[Age,Gender,Stream,Internships,CGPA,Hostel,HistoryOfBacklogs,projectsCount]])[0])
-    prediction_from_models.append(NBmodel.predict([[Age,Gender,Stream,Internships,CGPA,Hostel,HistoryOfBacklogs,projectsCount]])[0])
-    prediction_from_models.append(SVMmodel.predict([[Age,Gender,Stream,Internships,CGPA,Hostel,HistoryOfBacklogs,projectsCount]])[0])
-    prediction_from_models.append(RFmodel.predict([[Age,Gender,Stream,Internships,CGPA,Hostel,HistoryOfBacklogs,projectsCount]])[0])
-
+    prediction_from_models.append(LGmodel.predict([[Age, Gender, Stream, Internships, CGPA, Hostel, HistoryOfBacklogs, projectsCount]])[0])
+    prediction_from_models.append(KNNmodel.predict([[Age, Gender, Stream, Internships, CGPA, Hostel, HistoryOfBacklogs, projectsCount]])[0])
+    prediction_from_models.append(NBmodel.predict([[Age, Gender, Stream, Internships, CGPA, Hostel, HistoryOfBacklogs, projectsCount]])[0])
+    prediction_from_models.append(SVMmodel.predict([[Age, Gender, Stream, Internships, CGPA, Hostel, HistoryOfBacklogs, projectsCount]])[0])
+    prediction_from_models.append(RFmodel.predict([[Age, Gender, Stream, Internships, CGPA, Hostel, HistoryOfBacklogs, projectsCount]])[0])
 
     sum = 0
 
     predication_of_placement = ""
 
     for i in prediction_from_models:
-        sum+=i
+        sum += i
 
-    if sum==0 or sum ==1:
+    if sum == 0 or sum == 1:
         predication_of_placement = "You won't get placed."
     elif sum == 2:
         predication_of_placement = "You are most likely to not get placed."
@@ -76,11 +67,15 @@ def predict_placement(data:attrName):
     else:
         predication_of_placement = "Not Sure"
 
-    return {
-        'prediction_of_placement' : predication_of_placement,
-        'no_of_true' : sum
+
+    print(predication_of_placement)
+
+    value = {
+        'prediction_of_placement': predication_of_placement,
+        'no_of_true': int(sum)
     }
 
+    return value
 
 
 
